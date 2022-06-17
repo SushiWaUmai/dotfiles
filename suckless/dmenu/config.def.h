aa58e4132a8386810176c3d322c3561e3c322db8
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-17 17:42:14 +0200

    feat: :sparkles: add fuzzyhighlight patch

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -13,6 +13,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */

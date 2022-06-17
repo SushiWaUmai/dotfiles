dc046522c0b013afa044d8b286a9deaf398f292a
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-17 16:51:56 +0200

    feat: :sparkles: add bar padding to dwm

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -13,6 +13,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "MesloLGS Nerd Font Mono:size=10" };
 static const char dmenufont[]       = "MesloLGS Nerd Font Mono:size=10";
 

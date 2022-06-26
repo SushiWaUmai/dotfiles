c09cb966fc79362073c4c3d243464dd11f627185
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-26 20:43:49 +0200

    feat: :sparkles: change dwm font

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -11,7 +11,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 8;        /* 2 is the default spacing around the bar's font */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "MesloLGS Nerd Font Mono:size=12" };
+static const char *fonts[]          = { "MesloLGS Nerd Font:size=12" };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };

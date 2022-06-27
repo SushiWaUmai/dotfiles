f1fa789b5e0460e2230b5e35364d3f7e929b4c56
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-27 12:50:50 +0200

    feat: :sparkles: add xresources config in slock

--- suckless/slock/config.def.h
+++ suckless/slock/config.def.h
@@ -8,6 +8,16 @@ static const char *colorname[NUMCOLS] = {
 	[FAILED] = "#D08770",   /* wrong password */
 	[CAPS] =   "#B48EAD",   /* CapsLock on */
 };
+ 
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "init",         STRING,  &colorname[INIT] },
+		{ "input",        STRING,  &colorname[INPUT] },
+		{ "failed",       STRING,  &colorname[FAILED] },
+		{ "caps",         STRING,  &colorname[CAPS] },
+};
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;

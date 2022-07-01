25f16b26e3e21c769cfb01884adcc06dccb7e5c9
SushiWaUmai <eugene.bb1@gmail.com>
2022-07-01 22:12:27 +0200

    feat: :sparkles: create a simple install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# check if the user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+cd suckless/dmenu
+make clean install
+cd ../..
+
+cd suckless/dwm
+make clean install
+cd ../..
+
+cd suckless/slock
+make clean install
+cd ../..
+
+cd suckless/slstatus
+make clean install
+cd ../..
+
+cd suckless/st
+make clean install
+cd ../..
+
+cd suckless/surf
+make clean install
+cd ../..
+
+cd suckless/tabbed
+make clean install
+cd ../..
+
+# get the user name
+USERNAME=$(logname)
+cp -R ./.config /home/$USERNAME/
+cp -R ./.dwm /home/$USERNAME/
+cp -R ./.surf /home/$USERNAME/
+cp -R ./.wallpaper /home/$USERNAME/
+cp ./.xinitrc /home/$USERNAME/

4f78ef318b6dbbff01eddbe41638634757bb87b4
Eugene Matsumura <eugene.bb1@gmail.com>
2023-04-05 18:52:13 +0200

    feat: :sparkles: change surf keybindings to match more to chrome

--- suckless/surf/config.def.h
+++ suckless/surf/config.def.h
@@ -162,8 +162,8 @@ static Key keys[] = {
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_k,      zoom,       { .i = +1 } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_q,      zoom,       { .i = 0  } },
 	{ MODKEY,                GDK_KEY_minus,  zoom,       { .i = -1 } },
-	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_plus,   zoom,       { .i = +1 } },
-	{ MODKEY,                GDK_KEY_equal,  zoom,       { .i = 0  } },
+	{ MODKEY, 							 GDK_KEY_equal,  zoom,       { .i = +1 } },
+	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_plus,   zoom,       { .i = 0  } },
 
 	{ MODKEY,                GDK_KEY_p,      clipboard,  { .i = 1 } },
 	{ MODKEY,                GDK_KEY_y,      clipboard,  { .i = 0 } },

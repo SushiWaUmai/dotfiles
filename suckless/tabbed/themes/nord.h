71c7907fe2f052e2ab54bad98ac03d9d44a8b7df
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-29 00:07:14 +0200

    feat: remove surf and tabbed

--- suckless/tabbed/themes/nord.h
+++
@@ -1,8 +0,0 @@
-static const char* normbgcolor = "#2E3440";
-static const char* normfgcolor = "#88C0D0";
-
-static const char* selbgcolor = "#4C566A";
-static const char* selfgcolor = "#D08770";
-
-static const char* urgbgcolor = "#88C0D0";
-static const char* urgfgcolor = "#8FBCBB";

c563327d206bf9caa2559da80dfbe66c9c1605bc
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-29 10:35:08 +0200

    feat: enable control click on windows

--- suckless/chadwm/config.def.h
+++ suckless/chadwm/config.def.h
@@ -263,10 +263,10 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
   /* click                event mask      button          function        argument */
-    {ClkLtSymbol,   0,           Button1, setlayout,      {0}               },
-    {ClkLtSymbol,   0,           Button3, setlayout,      {.v = &layouts[2]}},
-    {ClkWinTitle,   0,           Button2, zoom,           {0}               },
-    {ClkStatusText, 0,           Button2, spawn,          SHCMD("st")       },
+    {ClkLtSymbol,   0,      Button1, setlayout,      {0}               },
+    {ClkLtSymbol,   0,      Button3, setlayout,      {.v = &layouts[2]}},
+    {ClkWinTitle,   0,      Button2, zoom,           {0}               },
+    {ClkStatusText, 0,      Button2, spawn,          SHCMD("st")       },
 
  /* Keep movemouse? */
   /* { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} }, */
@@ -281,18 +281,18 @@ static const Button buttons[] = {
   * to control these separately (i.e. to retain the feature to move a tiled window
   * into a floating position).
   */
-    {ClkClientWin,  MODKEY,      Button1, moveorplace,    {.i = 0}          },
-    {ClkClientWin,  MODKEY,      Button2, togglefloating, {0}               },
-    {ClkClientWin,  MODKEY,      Button3, resizemouse,    {0}               },
-    {ClkClientWin,  ControlMask, Button1, dragmfact,      {0}               },
-    {ClkClientWin,  ControlMask, Button3, dragcfact,      {0}               },
-    {ClkTagBar,     0,           Button1, view,           {0}               },
-    {ClkTagBar,     0,           Button3, toggleview,     {0}               },
-    {ClkTagBar,     MODKEY,      Button1, tag,            {0}               },
-    {ClkTagBar,     MODKEY,      Button3, toggletag,      {0}               },
-    {ClkTabBar,     0,           Button1, focuswin,       {0}               },
-    {ClkTabBar,     0,           Button1, focuswin,       {0}               },
-    {ClkTabPrev,    0,           Button1, movestack,      {.i = -1}         },
-    {ClkTabNext,    0,           Button1, movestack,      {.i = +1}         },
-    {ClkTabClose,   0,           Button1, killclient,     {0}               },
+    {ClkClientWin,  MODKEY, Button1, moveorplace,    {.i = 0}          },
+    {ClkClientWin,  MODKEY, Button2, togglefloating, {0}               },
+    {ClkClientWin,  MODKEY, Button3, resizemouse,    {0}               },
+ // {ClkClientWin,  ControlMask, Button1, dragmfact,      {0}               },
+  // {ClkClientWin,  ControlMask, Button3, dragcfact,      {0}               },
+    {ClkTagBar,     0,      Button1, view,           {0}               },
+    {ClkTagBar,     0,      Button3, toggleview,     {0}               },
+    {ClkTagBar,     MODKEY, Button1, tag,            {0}               },
+    {ClkTagBar,     MODKEY, Button3, toggletag,      {0}               },
+    {ClkTabBar,     0,      Button1, focuswin,       {0}               },
+    {ClkTabBar,     0,      Button1, focuswin,       {0}               },
+    {ClkTabPrev,    0,      Button1, movestack,      {.i = -1}         },
+    {ClkTabNext,    0,      Button1, movestack,      {.i = +1}         },
+    {ClkTabClose,   0,      Button1, killclient,     {0}               },
 };

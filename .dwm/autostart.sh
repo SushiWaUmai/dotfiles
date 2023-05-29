a5f51bdd52b97cc898f69cdb646881843222f017
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-30 00:31:39 +0200

    feat: update xss-lock command

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,7 +4,7 @@ exec slstatus &
 
 xset s on
 xset s 600
-xss-lock -n dm-tool lock
+xss-lock -- dm-tool lock &
 
 xwallpaper --focus "${HOME}/.wallpaper/wallpaper.png"
 
--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ pacman -Sy
 pacman -S base base-devel xorg imlib2 --noconfirm
 
 echo "Installing Tools"
-pacman -S pcmanfm shotgun xwallpaper picom autorandr xclip alacritty neovim rofi lxsession-gtk3 numlockx --noconfirm
+pacman -S pcmanfm shotgun xwallpaper picom autorandr xclip alacritty neovim rofi lxsession-gtk3 numlockx xss-lock --noconfirm
 
 echo "Compiling and Installing Suckless Tools..."
 make -C ./suckless/chadwm clean install

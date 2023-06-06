0125e09affa7662b7eb4355e0a10989d1eba7599
Eugene Matsumura <eugene.bb1@gmail.com>
2023-06-06 12:03:47 +0200

    feat: add a better installation script

--- install.sh
+++ install.sh
@@ -8,16 +8,40 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+
 echo "Installing Dependencies..."
 pacman -Sy
 pacman -S base base-devel xorg imlib2 --noconfirm
+pacman -S git curl --noconfirm
 
-echo "Installing Tools"
+
+echo "Installing Paru"
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru
+makepkg -si
+cd ..
+rm -rf ./paru-bin
+
+
+echo "Setting up zsh..."
+pacman -S zsh tmux --noconfirm
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+
+
+echo "Cloning dotfiles..."
+git clone https://github.com/SushiWaUmai/dotfiles.git --recursive
+cd dotfiles
+
+
+echo "Installing Tools..."
 pacman -S pcmanfm shotgun xwallpaper picom autorandr xclip alacritty neovim rofi lxsession-gtk3 numlockx xss-lock --noconfirm
+
 
 echo "Compiling and Installing Suckless Tools..."
 make -C ./suckless/chadwm clean install
 make -C ./suckless/slstatus clean install
+
 
 echo "Copying Configurations..."
 cp -R ./.dwm /home/$USER/

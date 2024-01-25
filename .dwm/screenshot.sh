d184ac214228300e4492ff591ad8e61f95a75532
Eugene Matsumura <eugene.bb1@gmail.com>
2024-01-25 23:20:46 +0100

    fix: remove screenshot commands

--- .dwm/screenshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-d="${HOME}/pics/screenshot-$(date +%Y-%m-%d-%H_%M-%S).png"
-
-
-shotgun -s $d
-xclip -t 'image/png' -sel c < $d
--- .dwm/screensnap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-d="${HOME}/pics/screenshot-$(date +%Y-%m-%d-%H_%M-%S).png"
-sel=$(slop -f "-i %i -g %g")
-
-shotgun $sel $d
-xclip -t 'image/png' -sel c < $d
